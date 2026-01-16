68e384e69e10d2ca0b8b9b79448a957659d65cac
LeeJaeWan <jjjkuul@naver.com>
2026-01-16 22:26:57 +0900

    별찍기 문제 완료

--- src/main/java/com/codesquad/mission1/StarPrinter.java
+++ src/main/java/com/codesquad/mission1/StarPrinter.java
@@ -1,4 +1,30 @@
 package com.codesquad.mission1;
 
+import java.util.Scanner;
+
 public class StarPrinter {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num;
+        System.out.print("별의 개수를 입력해 주세요 >>> ");
+        try {
+            num = scan.nextInt();
+        } catch (NumberFormatException e) {
+            System.out.println("숫자를 입력해주세요.");
+            return;
+        }
+
+        if(num < 0 || num > 100){
+            System.out.println("1 ~ 100만 입력해주세요!");
+            return;
+        }
+
+        for(int i = 1; i <= num; i++){
+            for(int j = 0; j < i; j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
 }
